424b2352f5adede8f0db8f1f4ce2c605458aad73
arsalaan.naeem <arsalaannaeem@hotmail.com>
2023-03-26 21:33:30 +0100

    updated

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -25,9 +25,24 @@ const getWorkout = async (req, res) => {
 	res.status(200).json(workout);
 };
 
-// post a new workout
+// create a new workout
 const createWorkout = async (req, res) => {
 	const { title, reps, load } = req.body;
+
+	// for handling errors on the frontend form
+	let emptyFields = [];
+	if (!title) {
+		emptyFields.push('title');
+	}
+	if (!load) {
+		emptyFields.push('load');
+	}
+	if (!reps) {
+		emptyFields.push('reps');
+	}
+	if (emptyFields.length > 0) {
+		return res.status(400).json({ error: 'Please fill in all the fields', emptyFields })
+	}
 
 	// add doc to db
 	try {
