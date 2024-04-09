740f1ab83e8095249f49a3cd91e3ab2bf7277660
Arsalaan N <arsalaannaeem@hotmail.com>
2024-04-09 01:11:46 +0100

    backend refactoring

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -1,14 +1,12 @@
 const Workout = require("../models/Workout");
 const mongoose = require("mongoose");
 
-// get all workouts
 const getWorkouts = async (req, res) => {
 	const workouts = await Workout.find({}).sort({ createdAt: -1 });
 
 	res.status(200).json(workouts);
 };
 
-// get a single workout
 const getWorkout = async (req, res) => {
 	const { id } = req.params;
 
@@ -25,23 +23,24 @@ const getWorkout = async (req, res) => {
 	res.status(200).json(workout);
 };
 
-// create a new workout
 const createWorkout = async (req, res) => {
 	const { title, reps, load } = req.body;
 
 	// for handling errors on the frontend form
 	let emptyFields = [];
 	if (!title) {
-		emptyFields.push('title');
+		emptyFields.push("title");
 	}
 	if (!load) {
-		emptyFields.push('load');
+		emptyFields.push("load");
 	}
 	if (!reps) {
-		emptyFields.push('reps');
+		emptyFields.push("reps");
 	}
 	if (emptyFields.length > 0) {
-		return res.status(400).json({ error: 'Please fill in all the fields', emptyFields })
+		return res
+			.status(400)
+			.json({ error: "Please fill in all the fields", emptyFields });
 	}
 
 	// add doc to db
@@ -53,7 +52,6 @@ const createWorkout = async (req, res) => {
 	}
 };
 
-// delete a workout
 const deleteWorkout = async (req, res) => {
 	const { id } = req.params;
 
@@ -70,7 +68,6 @@ const deleteWorkout = async (req, res) => {
 	res.status(200).json(workout);
 };
 
-// update a workout
 const updateWorkout = async (req, res) => {
 	const { id } = req.params;
 
@@ -82,7 +79,8 @@ const updateWorkout = async (req, res) => {
 		{ _id: id },
 		{
 			...req.body,
-		}
+		},
+		{ new: true }
 	);
 
 	if (!workout) {
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -9,19 +9,10 @@ const {
 
 const router = express.Router();
 
-// GET all workouts
 router.get("/", getWorkouts);
-
-// GET a single workout
 router.get("/:id", getWorkout);
-
-// POST a new workout
 router.post("/", createWorkout);
-
-// DELETE a workout
 router.delete("/:id", deleteWorkout);
-
-// UPDATE a workout
 router.patch("/:id", updateWorkout);
 
 module.exports = router;
