70ce9fc4a4df10d39ab7d146ec097c4f4b88e2e5
Arsalaan N <arsalaannaeem@hotmail.com>
2024-04-10 02:13:52 +0100

    completed auth on backend & frontend

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -1,8 +1,9 @@
-const Workout = require("../models/Workout");
+const Workout = require("../models/workoutModel");
 const mongoose = require("mongoose");
 
 const getWorkouts = async (req, res) => {
-	const workouts = await Workout.find({}).sort({ createdAt: -1 });
+	const user_id = req.user._id;
+	const workouts = await Workout.find({ user_id }).sort({ createdAt: -1 });
 
 	res.status(200).json(workouts);
 };
@@ -45,7 +46,8 @@ const createWorkout = async (req, res) => {
 
 	// add doc to db
 	try {
-		const workout = await Workout.create({ title, reps, load });
+		const user_id = req.user._id;
+		const workout = await Workout.create({ title, reps, load, user_id });
 		res.status(201).json(workout);
 	} catch (error) {
 		res.status(400).json({ error: error.message });
